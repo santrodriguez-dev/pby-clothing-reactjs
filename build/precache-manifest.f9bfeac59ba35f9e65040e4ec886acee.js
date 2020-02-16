71382dd5777001bf3f15fafde93da60085b190ea
Santiago Rodríguez <srodriguezm95@gmail.com>
2020-02-15 23:01:28 -0500

    build

---
+++ build/precache-manifest.f9bfeac59ba35f9e65040e4ec886acee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d951c096f38a67aeb1762a309e2134a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56f11333765b09c07652",
+    "url": "/static/css/main.390b363a.chunk.css"
+  },
+  {
+    "revision": "766607beb088271a66fd",
+    "url": "/static/js/2.d20cb7b2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d20cb7b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f11333765b09c07652",
+    "url": "/static/js/main.2935094d.chunk.js"
+  },
+  {
+    "revision": "8e7a607a1527c520cc65",
+    "url": "/static/js/runtime-main.1e486441.js"
+  }
+]);
