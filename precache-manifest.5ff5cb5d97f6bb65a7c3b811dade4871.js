d06854593331be043eaefd84f34f94ff9f8f154d
Santiago Rodríguez <srodriguezm95@gmail.com>
2020-02-15 23:19:37 -0500

    Updates

---
+++ precache-manifest.5ff5cb5d97f6bb65a7c3b811dade4871.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ffb0f7913b3083f45df124a1b424151",
+    "url": "/pby-clothing-reactjs/index.html"
+  },
+  {
+    "revision": "0910252e5c23a23ac2d0",
+    "url": "/pby-clothing-reactjs/static/css/main.390b363a.chunk.css"
+  },
+  {
+    "revision": "766607beb088271a66fd",
+    "url": "/pby-clothing-reactjs/static/js/2.d20cb7b2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pby-clothing-reactjs/static/js/2.d20cb7b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0910252e5c23a23ac2d0",
+    "url": "/pby-clothing-reactjs/static/js/main.a164ee32.chunk.js"
+  },
+  {
+    "revision": "deba0f5d7102a2549203",
+    "url": "/pby-clothing-reactjs/static/js/runtime-main.19087515.js"
+  }
+]);
