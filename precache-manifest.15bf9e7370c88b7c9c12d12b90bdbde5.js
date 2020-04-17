2e456063477feffb4fed5f95a945d12a32b16669
Santiago Rodríguez <srodriguezm95@gmail.com>
2020-04-17 16:07:31 -0500

    Updates

---
+++ precache-manifest.15bf9e7370c88b7c9c12d12b90bdbde5.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd49f206fde99ed74c31798aaad8ff6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "800f07c8759717a089f6",
+    "url": "./static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "221559dfdae61721fad3",
+    "url": "./static/css/main.322176c1.chunk.css"
+  },
+  {
+    "revision": "800f07c8759717a089f6",
+    "url": "./static/js/2.dc3962e9.chunk.js"
+  },
+  {
+    "revision": "0897a2af4d47251d787cefe2723205c2",
+    "url": "./static/js/2.dc3962e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221559dfdae61721fad3",
+    "url": "./static/js/main.65defdb4.chunk.js"
+  },
+  {
+    "revision": "eaac0ff927e408119409",
+    "url": "./static/js/runtime-main.5f8d941b.js"
+  },
+  {
+    "revision": "d6c52cd829ac09aad08b8214a2b2b463",
+    "url": "./static/media/1.d6c52cd8.jpg"
+  },
+  {
+    "revision": "e983a452388b5c3b3d2ce5ce55121e35",
+    "url": "./static/media/1.e983a452.jpg"
+  },
+  {
+    "revision": "2cf7bb44b6dbf6eb8391f1d7415b6b79",
+    "url": "./static/media/2.2cf7bb44.jpg"
+  },
+  {
+    "revision": "66e8f64a388829e28a9ceaed668a4f15",
+    "url": "./static/media/2.66e8f64a.jpg"
+  },
+  {
+    "revision": "115b1054c412e8db9509ffa35a12c6ae",
+    "url": "./static/media/3.115b1054.jpg"
+  },
+  {
+    "revision": "2a662c2f988a9be6e0fd0e9e9597d2af",
+    "url": "./static/media/3.2a662c2f.jpg"
+  },
+  {
+    "revision": "7b2202d4199225471b44efbd2ca82499",
+    "url": "./static/media/4.7b2202d4.jpg"
+  },
+  {
+    "revision": "ca884f30fc118075f647f23a63021359",
+    "url": "./static/media/4.ca884f30.jpg"
+  },
+  {
+    "revision": "b0ec29be799f3ccf0f5a03610c7d6fc3",
+    "url": "./static/media/5.b0ec29be.jpg"
+  },
+  {
+    "revision": "d048028cff6b491785c3d7978637d348",
+    "url": "./static/media/6.d048028c.jpg"
+  },
+  {
+    "revision": "c29ca1df0cac5eb24d62d64e9734026d",
+    "url": "./static/media/Montserrat-ExtraBold.c29ca1df.otf"
+  },
+  {
+    "revision": "b7a0bca87b664f3155d5120d9990ed63",
+    "url": "./static/media/Montserrat-Medium.b7a0bca8.otf"
+  },
+  {
+    "revision": "b041170eac8282c54ad3311879970d2b",
+    "url": "./static/media/Montserrat-Regular.b041170e.otf"
+  },
+  {
+    "revision": "02ee4bf3ae4d477b48e90a7987458d45",
+    "url": "./static/media/Montserrat-SemiBold.02ee4bf3.otf"
+  },
+  {
+    "revision": "f595067b8fd8b5b7223ebbf65a2341dc",
+    "url": "./static/media/PopUp.f595067b.jpg"
+  },
+  {
+    "revision": "b527ac873ed95eb656fa545151065d70",
+    "url": "./static/media/img1.b527ac87.webp"
+  },
+  {
+    "revision": "a2cbf607b9dbf648eff1ff579825b5bf",
+    "url": "./static/media/img3.a2cbf607.webp"
+  },
+  {
+    "revision": "fc680ec99329054aeebe90d7451a3761",
+    "url": "./static/media/img4.fc680ec9.webp"
+  },
+  {
+    "revision": "8b11ab01810bc33f0eff1886e54fff12",
+    "url": "./static/media/logo1.8b11ab01.png"
+  },
+  {
+    "revision": "ec953ddf1602bea3b4e9f12040d54981",
+    "url": "./static/media/logo2.ec953ddf.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.abfccadf72f4a8e17e03ab9d18f72af0.js"
+  "./precache-manifest.15bf9e7370c88b7c9c12d12b90bdbde5.js"
 );
 
 self.addEventListener('message', (event) => {
