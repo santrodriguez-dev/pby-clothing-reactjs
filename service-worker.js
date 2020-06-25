03d04be49c9e98cd39d21de7cfce45a726f44e45
Santiago Rodríguez <srodriguezm95@gmail.com>
2020-06-25 08:22:41 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.85cf677662f7d557cff3ce2677d1547c.js"
+  "./precache-manifest.084a131f54e3e1dab65bd1e593ca78a2.js"
 );
 
 self.addEventListener('message', (event) => {
