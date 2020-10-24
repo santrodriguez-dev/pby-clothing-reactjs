553bae65c8596bc76d09c0b87643e40091f4e544
Santiago Rodriguez <srodriguezm95@gmail.com>
2020-10-24 14:27:57 -0500

    ajuste redux para publicacion

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,8 +6,8 @@ import reducer from './reducers'
 const store = createStore(
   reducer,
   compose(applyMiddleware(thunk),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    // (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    // (window as any).__REDUX_DEVTOOLS_EXTENSION__()
   )
 )
 
